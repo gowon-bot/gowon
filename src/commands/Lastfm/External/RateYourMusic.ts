2fae6baeaf6f2fc742db2b3cd32f231dea216d74
jivison <john@ivison.org>
2020-10-27 23:48:36 -0700

    Add wikipedia, whosampled, rateyourmusic commands

---
+++ src/commands/Lastfm/External/RateYourMusic.ts
@@ -0,0 +1,43 @@
+import { MessageEmbed } from "discord.js";
+import { Arguments } from "../../../lib/arguments/arguments";
+import { standardMentions } from "../../../lib/arguments/mentions/mentions";
+import { LastFMBaseCommand } from "../LastFMBaseCommand";
+
+export default class RateYourMusic extends LastFMBaseCommand {
+  aliases = ["ryms"];
+  description = "Search rateyourmusic for an album (or anything!)";
+  subcategory = "external";
+
+  arguments: Arguments = {
+    inputs: {
+      keywords: { index: { start: 0 } },
+    },
+    mentions: standardMentions,
+  };
+
+  async run() {
+    let keywords = this.parsedArguments.keywords as string;
+
+    let { username } = await this.parseMentions({
+      usernameRequired: !keywords,
+    });
+
+    if (!keywords) {
+      let nowplaying = await this.lastFMService.nowPlayingParsed(username);
+
+      keywords = `${nowplaying.artist} - ${nowplaying.album}`;
+    }
+
+    let encodedKeywords = encodeURIComponent(keywords);
+
+    let embed = new MessageEmbed()
+      .setAuthor(`Rateyourmusic search for "${keywords}"`)
+      .setTitle("Click here to view the results")
+      .setURL(
+        `https://www.google.com/search?q=${encodedKeywords}+site%3Arateyourmusic.com`
+      )
+      .setThumbnail("https://e.snmc.io/3.0/img/logo/sonemic-512.png");
+
+    await this.send(embed);
+  }
+}
