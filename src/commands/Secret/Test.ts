0df973d831199bc8a99b182543db3cb62f07b6fd
jivison <john@ivison.org>
2020-11-24 02:13:04 -0800

    Add simple queue, scrobbling

--- src/commands/Secret/Test.ts
+++ src/commands/Secret/Test.ts
@@ -1,14 +1,8 @@
-import { sleep } from "../../helpers";
 import { BaseCommand } from "../../lib/command/BaseCommand";
 
 export default class Test extends BaseCommand {
   description = "Testing testing 123";
   secretCommand = true;
-
-  async testFunction(number: number): Promise<number> {
-    await sleep(number);
-    return number;
-  }
 
   async run() {
     await this.send("Hello, world!");
