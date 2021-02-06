f1bb1ea190b193232b2392af941808afe03273d5
jivison <john@ivison.org>
2021-02-06 13:07:42 -0800

    Remove id searching with username

--- src/commands/Meta/ID.ts
+++ src/commands/Meta/ID.ts
@@ -3,9 +3,6 @@ import { Arguments } from "../../lib/arguments/arguments";
 import { standardMentions } from "../../lib/arguments/mentions/mentions";
 
 const args = {
-  inputs: {
-    discordUsername: { index: { start: 0 } },
-  },
   mentions: standardMentions,
 } as const;
 
@@ -18,27 +15,12 @@ export default class ID extends BaseCommand<typeof args> {
   arguments: Arguments = args;
 
   async run() {
-    const username = this.parsedArguments.discordUsername;
-
     let { discordUser } = await this.parseMentions({
       fetchDiscordUser: true,
       reverseLookup: { lastFM: true },
       usernameRequired: false,
     });
 
-    if (username) {
-      const user = this.message.guild?.members.cache.find(
-        (member) => member.user.username === username
-      );
-
-      if (!user)
-        this.reply(
-          "couldn't find that user. " +
-            "Note that username is case sensitive".italic()
-        );
-      else await this.send(user?.id);
-    } else {
-      await this.send(discordUser!.id);
-    }
+    await this.send(discordUser!.id);
   }
 }
