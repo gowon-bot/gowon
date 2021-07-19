f98a6b69795dbf498a4f347e431ada4ffb0c8b0d
jivison <john@ivison.org>
2021-07-19 15:24:21 -0700

    Add around command

---
+++ src/commands/Lastfm/Rank/ArtistAround.ts
@@ -0,0 +1,95 @@
+import { Arguments } from "../../../lib/arguments/arguments";
+import { LastFMBaseCommand } from "../LastFMBaseCommand";
+import { standardMentions } from "../../../lib/arguments/mentions/mentions";
+import { RedirectsCache } from "../../../lib/caches/RedirectsCache";
+import { RedirectsService } from "../../../services/dbservices/RedirectsService";
+import {
+  displayNumber,
+  displayNumberedList,
+} from "../../../lib/views/displays";
+import { LogicError } from "../../../errors";
+
+const args = {
+  inputs: {
+    artist: {
+      index: { start: 0 },
+    },
+  },
+  mentions: standardMentions,
+} as const;
+
+export default class ArtistAround extends LastFMBaseCommand<typeof args> {
+  idSeed = "hot issue dain";
+
+  aliases = ["around", "aar"];
+  description = "Shows the ranks around an artist is in your top 1000 artists";
+  subcategory = "ranks";
+  usage = ["artist @user"];
+
+  arguments: Arguments = args;
+
+  redirectsService = new RedirectsService(this.logger);
+  redirectsCache = new RedirectsCache(this.redirectsService);
+
+  async run() {
+    const { requestable, senderRequestable, perspective } =
+      await this.parseMentions({
+        senderRequired: !this.parsedArguments.artist,
+      });
+
+    const artistName = await this.lastFMArguments.getArtist(
+      senderRequestable,
+      true
+    );
+
+    const topArtists = await this.lastFMService.topArtists({
+      username: requestable,
+      limit: 1000,
+    });
+
+    const rank = (
+      await Promise.all(
+        topArtists.artists.map(
+          async (a) => await this.redirectsCache.getRedirect(a.name)
+        )
+      )
+    ).findIndex((a) => a.toLowerCase() === artistName.toLowerCase());
+
+    if (rank === -1) {
+      throw new LogicError(
+        `That artist wasn't found in ${
+          perspective.possessive
+        } top ${displayNumber(topArtists.artists.length, "artist")}`
+      );
+    }
+
+    console.log(rank);
+
+    const start = rank < 5 ? 0 : rank - 5;
+    const stop =
+      rank > topArtists.artists.length - 6
+        ? topArtists.artists.length - 1
+        : rank + 6;
+
+    const embed = this.newEmbed()
+      .setAuthor(...this.generateEmbedAuthor("Artist around"))
+      .setTitle(
+        `Artists around ${topArtists.artists[rank].name} in ${perspective.possessive} library`
+      )
+      .setDescription(
+        displayNumberedList(
+          topArtists.artists.slice(start, stop).map((val, idx) => {
+            const display = `${val.name} - ${displayNumber(
+              val.userPlaycount,
+              "play"
+            )}`;
+
+            return start + idx === rank ? display.strong() : display;
+          }),
+          start
+        )
+      );
+
+    await this.send(embed);
+  }
+}
