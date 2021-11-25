0ac634f676178e3febb197d741da5a68b5e610c0
jivison <john@ivison.org>
2021-11-25 00:40:27 -0800

    ping pong

--- src/commands/Secret/Ping.ts
+++ src/commands/Secret/Ping.ts
@@ -1,4 +1,5 @@
 import { Chance } from "chance";
+import { shuffle } from "../../helpers";
 import { BaseCommand } from "../../lib/command/BaseCommand";
 
 export default class Ping extends BaseCommand {
@@ -6,11 +7,36 @@ export default class Ping extends BaseCommand {
 
   subcategory = "developer";
   description = "Ping! Pong!";
-  aliases = ["🏓", "pin", "pingu", "pung", "peng"];
+  aliases = ["🏓", "pin", "pingu", "pung", "peng", "핑", "pingmatrix"];
   secretCommand = true;
 
   async run() {
-    if (this.runAs.variationWasUsed("pung")) {
+    if (this.runAs.variationWasUsed("pingmatrix")) {
+      const matrix = [] as number[][];
+
+      const rows = shuffle(["p", "n", "g", "o", "!"]);
+      const columns = shuffle(["p", "n", "g", "o", "!"]);
+
+      for (let rowIndex = 0; rowIndex < rows.length; rowIndex++) {
+        const row = rows[rowIndex];
+        for (let columnIndex = 0; columnIndex < columns.length; columnIndex++) {
+          const column = columns[columnIndex];
+
+          if (!matrix[rowIndex]) matrix[rowIndex] = [];
+
+          matrix[rowIndex][columnIndex] = row === column ? 1 : 0;
+        }
+      }
+
+      await this.send(`\`\`\`
+    ${columns.join(" ")}
+  / ${" ".repeat(columns.join(" ").length)} \\
+${matrix
+  .map((row, idx) => rows[idx] + " |" + " " + row.join(" ") + " |")
+  .join("\n")}
+  \\ ${" ".repeat(columns.join(" ").length)} /
+\`\`\``);
+    } else if (this.runAs.variationWasUsed("pung")) {
       // PUNG! by Yukika
       await this.reply(
         "https://open.spotify.com/track/2YnPNuWGG3zlwyUyG0hBOd",
@@ -19,10 +45,14 @@ export default class Ping extends BaseCommand {
     } else if (this.runAs.variationWasUsed("peng")) {
       await this.reply("ting", { noUppercase: true });
     } else if (
-      this.runAs.variationWasUsed("ping") &&
+      this.runAs.variationWasUsed("ping", "핑") &&
       Chance().bool({ likelihood: 20 })
     ) {
-      await this.reply("PANG! 🏌️");
+      await this.reply(
+        this.runAs.variationWasUsed("핑") ? "팡! 🏌️" : "PANG! 🏌️"
+      );
+    } else if (this.runAs.variationWasUsed("핑")) {
+      await this.reply("퐁");
     } else {
       await this.reply(
         `Pon${
--- src/commands/Secret/Pong.ts
+++ src/commands/Secret/Pong.ts
@@ -4,11 +4,18 @@ import { Emoji } from "../../lib/Emoji";
 export default class Pong extends BaseCommand {
   idSeed = "2ne1 dara";
 
+  aliases = ["nop"];
+
   subcategory = "fun";
   description = "!gnoP !gniP";
   secretCommand = true;
 
   async run() {
-    await this.reply(`${Emoji.gnop} gniP`, { ping: true });
+    await this.reply(
+      `${Emoji.gnop} ${!this.runAs.variationWasUsed("nop") ? "g" : ""}niP`,
+      {
+        ping: true,
+      }
+    );
   }
 }
