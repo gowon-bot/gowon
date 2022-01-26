05f3f2f4af28e6cbf2cdf6813130178e32b6e53d
jivison <john@ivison.org>
2022-01-25 16:39:58 -0800

    Add syncbannedtags script

---
+++ src/scripts/SyncBannedTags.ts
@@ -0,0 +1,33 @@
+import { Message } from "discord.js";
+import fetch from "node-fetch";
+import streamToString from "stream-to-string";
+import { LogicError } from "../errors";
+import { BaseServiceContext } from "../services/BaseService";
+import { ServiceRegistry } from "../services/ServicesRegistry";
+import { WordBlacklistService } from "../services/WordBlacklistService";
+
+export default async function syncBannedTags({
+  ctx,
+  message,
+}: {
+  ctx: BaseServiceContext;
+  message: Message;
+}) {
+  const blacklistService = ServiceRegistry.get(WordBlacklistService);
+  const wordsFile = message.attachments.first();
+
+  if (!wordsFile)
+    throw new LogicError("Please attach a list of words to blacklist!");
+
+  const file = await fetch(wordsFile.url);
+
+  const fileContent = await streamToString(file.body);
+
+  for (const word of fileContent.split("\n")) {
+    if (word) {
+      try {
+        await blacklistService.serverBanTag(ctx, word);
+      } catch {}
+    }
+  }
+}
