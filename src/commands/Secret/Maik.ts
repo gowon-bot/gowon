41ab9a56f3cefc2a2ce3b7c51ebfdebf6d0976ba
jivison <john@ivison.org>
2022-02-02 13:56:24 -0800

    Nothing to see here

---
+++ src/commands/Secret/Maik.ts
@@ -0,0 +1,25 @@
+import { shuffle } from "../../helpers";
+import { BaseCommand } from "../../lib/command/BaseCommand";
+
+export default class Maik extends BaseCommand {
+  idSeed = "billlie tsuki";
+
+  subcategory = "fun";
+  description = `"John can you add a maik command to gowon"\n"ok"`;
+  secretCommand = true;
+
+  async run() {
+    await this.send(shuffle(quotes)[0]);
+  }
+}
+
+const quotes: string[] = [
+  "To the joker, jopping is just regular hopping",
+  "listen up You guys who's mentally sick & ill. I'm not Gorgeous, pretty, king ,god .... Princess..💕 🎀 💝",
+  "Good that freak doesnt deserve a penny",
+  "Ayo. Is Mr worldwide. listen up You guys who's mentally sick & ill. I'm not gay. But be careful with those words. Respect LGBT people and just live your worst life well. I'm praying for you everyday. Cuz your life is so poor n so broke that you don't even know the words of happy and joyful^^. Dale\n*beat drops*",
+  "Maybe... i am a white boy",
+  "https://media.discordapp.net/attachments/769118965560770581/877664595446202418/E9Ffy5AXoAooXZw.png",
+  "I got a pic of my cat next to the penis vinyl somewhere",
+  "Wanna know my credit card number",
+];
