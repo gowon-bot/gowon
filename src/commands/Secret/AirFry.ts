b0e42858ead5158236127899eaf3fec4e34c113f
jivison <john@ivison.org>
2022-03-14 18:39:30 -0700

    Please gowon, air fry

--- src/commands/Secret/AirFry.ts
+++ src/commands/Secret/AirFry.ts
@@ -4,11 +4,11 @@ export default class AirFry extends BaseCommand {
   idSeed = "ive rei";
 
   subcategory = "fun";
-  description = "Ping! Pong! Pang?";
+  description = "soyeon air fryer";
   secretCommand = true;
 
   async run() {
-    await this.reply(
+    await this.send(
       "cdn.discordapp.com/attachments/768596255697272865/953103223751839844/1503414000255655944_01.mp4"
     );
   }
