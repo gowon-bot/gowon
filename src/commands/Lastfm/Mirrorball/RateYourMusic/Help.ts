bd3a060ad994130ef1b49f26284f1fd57ecb9518
John <jri1408@gmail.com>
2023-08-17 23:54:13 -0700

    Fix rym import help, remove unneeded packages

--- src/commands/Lastfm/Mirrorball/RateYourMusic/Help.ts
+++ src/commands/Lastfm/Mirrorball/RateYourMusic/Help.ts
@@ -12,12 +12,8 @@ export class Help extends RateYourMusicChildCommand {
       .setAuthor(this.generateEmbedAuthor("Rateyourmusic import help"))
       .setDescription(
         `You can export your rateyourmusic data by going to your profile page, and at the very botton of the page clicking the "Export your data" button.
-        
-To import your ratings, you can do one of two things:
 
-- Copy paste the ratings, then visit https://gowon.ca/import-ratings to import them (works on mobile!)
-
-- Copy the entire page, then paste it into Discord with \`${this.prefix}rymimport \`, sDiscord will convert it to a file for you (if not Gowon will still handle it)`
+Once completing the Captcha and downloading the \`.csv\` file, upload it to discord along with \`${this.prefix}rymimport\``
       );
 
     await this.send(embed);
