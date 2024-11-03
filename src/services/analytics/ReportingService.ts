04f61f130c139a17abaed33f6084bc352d77bf86
John Ivison <john@ivison.org>
2024-11-03 01:06:32 -0800

    Remove sentry

--- src/services/analytics/ReportingService.ts
+++ src/services/analytics/ReportingService.ts
@@ -1,40 +1,8 @@
-import * as Sentry from "@sentry/node";
-import config from "../../../config.json";
-import { ClientError } from "../../errors/errors";
 import { GowonContext } from "../../lib/context/Context";
 import { BaseService } from "../BaseService";
 
 export class ReportingService extends BaseService {
-  async init() {
-    Sentry.init({
-      dsn: config.sentryDSN,
-    });
-  }
+  async init() {}
 
-  public reportError(ctx: GowonContext, e: Error): void {
-    Sentry.withScope((scope) => {
-      this.addContextToScope(ctx, scope);
-
-      if (e instanceof ClientError) {
-        scope.setLevel("log");
-      }
-
-      Sentry.captureException(e);
-    });
-  }
-
-  private addContextToScope(ctx: GowonContext, scope: Sentry.Scope) {
-    scope.setTag("command-name", ctx.command?.friendlyNameWithParent);
-
-    scope.setUser({
-      id: ctx.author.id,
-      username: ctx.author.username,
-    });
-
-    scope.setExtra("user", {
-      discord: { id: ctx.author.id, username: ctx.author.username },
-    });
-    scope.setExtra("arguments", ctx.command.parsedArguments ?? {});
-    scope.setExtra("guild-id", ctx.guild?.id ?? "DM");
-  }
+  public reportError(_ctx: GowonContext, _e: Error): void {}
 }
